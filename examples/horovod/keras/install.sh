a7c54d7a40c02bd032be0e3b948e1d5da36f2b58
ohtaman <ohtamans@gmail.com>
2019-03-02 09:42:34 +0900

    Update install.sh

--- examples/horovod/keras/install.sh
+++ examples/horovod/keras/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#$-l rt_F=1
+#$-j y
+#$-cwd
 
-
+# Environment Modules の初期化
+source /etc/profile.d/modules.sh
+# Environment Modules の設定
 module load openmpi/2.1.5 python/3.6/3.6.5 cuda/9.0/9.0.176.4 cudnn/7.2/7.2.1 nccl/2.3/2.3.7-1
 export LD_LIBRARY_PATH=$CUDA_HOME/extras/CUPTI/lib64:$LD_LIBRARY_PATH
 python3 -m venv venv
--- examples/horovod/tf/install.sh
+++ examples/horovod/tf/install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+#$-l rt_F=1
+#$-j y
+#$-cwd
 
-
-module load python/3.6 cuda/9.0 cudnn/7.2 nccl/2.3
-python3 -m venv venv 
+# Environment Modules の初期化
+source /etc/profile.d/modules.sh
+# Environment Modules の設定
+module load openmpi/2.1.5 python/3.6/3.6.5 cuda/9.0/9.0.176.4 cudnn/7.2/7.2.1 nccl/2.3/2.3.7-1
+export LD_LIBRARY_PATH=$CUDA_HOME/extras/CUPTI/lib64:$LD_LIBRARY_PATH
+python3 -m venv venv
 source venv/bin/activate
 
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
