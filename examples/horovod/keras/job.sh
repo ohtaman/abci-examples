637a9134d868dae11bf4b57fbce0aef995cadf14
ohtaman <ohtamans@gmail.com>
2019-03-02 09:05:12 +0900

    Update job.sh

--- examples/horovod/keras/job.sh
+++ examples/horovod/keras/job.sh
@@ -12,10 +12,10 @@ HOSTFILE=hosts
 source /etc/profile.d/modules.sh
 # Environment Modules の設定
 module load openmpi/2.1.5 python/3.6/3.6.5 cuda/9.0/9.0.176.4 cudnn/7.2/7.2.1 nccl/2.3/2.3.7-1 
-#export LD_LIBRARY_PATH=$CUDA_HOME/extras/CUPTI/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/extras/CUPTI/lib64:$LD_LIBRARY_PATH
 
 python3 -m venv venv
 source venv/bin/activate
 
 # プログラムの実行
-mpirun -N N_GPU python3 mnist.py
+mpirun -N $N_GPU python3 mnist.py
